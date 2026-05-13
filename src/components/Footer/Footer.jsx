d789072b7785fe038abbc43f4160c399b38d74ab
devisonsantana <devisonsantana@hotmail.com>
2026-05-12 21:38:06 -0300

    feat: add footer information

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,37 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className={style.Footer}>
-      <p>Copyright &copy; {currentYear}</p>
-      <img src={Image} alt="sp1d3r" />
+      <div className={style.Copy}>
+        <p>Copyright &copy; {currentYear}</p>
+        <img src={Image} alt="sp1d3r" />
+      </div>
+      <div className={style.Container}>
+        <div className={style.Container__Tech}>
+          <p>
+            Desenvolvido com <span>React</span> & <span>ASP.NET</span>
+          </p>
+        </div>
+        <div className={style.Container__Host}>
+          <p>
+            Front-end hospedado na{" "}
+            <a href="https://vercel.com/" target="_blank">
+              Vercel
+            </a>
+          </p>
+          <p>
+            Back-end ASP.NET hospedado no{" "}
+            <a href="https://render.com/" target="_blank">
+              Render
+            </a>
+          </p>
+          <p>
+            Banco de dados{" "}
+            <a href="https://mongodb.com/" target="_blank">
+              MongoDb
+            </a>
+          </p>
+        </div>
+      </div>
     </footer>
   );
 };
